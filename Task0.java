7a6cee05e932afb574aa5d603b80d4bd46e8f7c2
Alexandra Shevchenko <alexshevchenko95@gmail.com>
2023-07-08 10:46:42 +0300

    task1

--- Task0.java
+++ Task0.java
@@ -17,7 +17,7 @@
 
 </aside>
  */
-public class Task0 {
+/*public class Task0 {
 
   public static void main(String[] args) {
     task0();
@@ -38,4 +38,39 @@ public class Task0 {
 
 
   }
+} */
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Passport {
+    private Map<String, String> map = new HashMap<>();
+
+    void add(String passNum, String lastName){
+        map.put(passNum,lastName);
+    }
+
+    String getByPassNum(String passNum){
+        return passNum + " : " + map.get(passNum);
+    }
+
+    String getByLastName(String lastName){
+        StringBuilder stringBuilder = new StringBuilder();
+        for (Map.Entry entry:
+             map.entrySet()) {
+            if(entry.getValue().equals(lastName)){
+                stringBuilder.append(entry.getKey());
+                stringBuilder.append(" : ");
+                stringBuilder.append(entry.getValue());
+                stringBuilder.append("\n");
+            }
+            
+        }
+        return stringBuilder.toString();
+    }
+
+    String getAll(){
+        return map.toString();
+    }
+
 }
